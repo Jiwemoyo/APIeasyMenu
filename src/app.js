2680166fa8958f5272e7488d0760cf8578c1b8c3
Mishelle126 <97122505+Mishelle126@users.noreply.github.com>
2024-06-17 18:10:03 -0500

    docker

--- src/app.js
+++ src/app.js
@@ -3,15 +3,21 @@ import { config } from 'dotenv.js';
 import bodyParser from 'body-parser.js';
 import  connectDB from './config/dbConfig.js';
 import  recipeRoutes from './routes/recipeRoutes.js';
+const mongoose = require("mongoose")
 
 //importar para el typo modulo
-
+const commentRoutes = require("./routes/commentRoutes.js")
 config();
 
 //lo otro que falta es la conexion al servidor esa esta hecha en server.js
 const app = express();
 app.use(bodyParser.json());
 
+//conectar la base
+mongoose.connect(process.env.MONGO_URL,{dbName: process.env.MONGO_DB_NAME})
+const db = mongoose.connection;
+
+app.use("/comment", commentRoutes)
 //Toto esto de conectar la base de datos esta hecho en el archivo config dentro de dbConfig.js
 connectDB();
 
--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -1,3 +1,82 @@
  const express = require("express")
  const router = express.router()
  const Comment = required("../models/commentModel.js")
+
+ //middlewere
+ const getComment = async (req, res, next) => {
+    let comment;
+    const { id } = req.params;
+
+    if(!id.macth(/^[0-9a-fA-F]{24}$/)){
+        return res.status(404).json (
+            {
+                message: "el comentario no es válido"
+            }
+        )
+    }
+    try {
+        comment = await Comment.findId(id)
+        if(!comment){
+            return res.status(404).json(
+                {
+                    messae: "el comentario no fue encontrado"
+                }
+            )
+        }
+    } catch (error) {
+        return res.status(500).json(
+            {
+                message: error.message
+            }
+        )
+    }
+    res.comment = comment;
+    next();
+ }
+
+ // obtener todos los comentarios[get all]
+
+ router.get("/", async (req, res)=> {
+    try {
+        const comment = await Comment.final(); 
+        consol.log("FET ALL", comment)
+        if (comment.length === 0) {
+           return  res.status(204).json([])
+        }
+        res(comment)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+ })
+
+ // crear un nuevo libro crear recursos [post]
+router.post("/", async (req, res) => {
+    const{author, title, postId, publication_date, likes, replies} = req?.body
+    if(!author || !title|| !postId || !publication_date || !likes || !replies){
+        return res.status(400).json({
+            message: "los campos autor, titulo, id, date, likes, replies son obligatorios"
+        })
+    }
+
+    const comment = new Comment (
+        {
+            author, 
+            title, 
+            postId, 
+            publication_date, 
+            likes, 
+            replies
+
+        }
+    )
+    try {
+        const newComment = await comment.save()
+        console.log(newComment)
+        res.status(201).json(newComment)
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+        
+    }
+}) 
