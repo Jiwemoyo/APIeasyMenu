7edb3f8093bf95a26c9d7e419ca9d5426bdba45b
Aless4ndr0 <jiwemoyo@gmail.com>
2024-07-25 14:00:38 -0500

    hice algunos ajustes aqui

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,15 @@
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
-    const { username, email, password, role } = req.body; // Añade 'role' en el cuerpo de la solicitud
-    try {
-        const user = new User({ username, email, password, role }); // Guarda el rol del usuario
-        await user.save();
-        res.status(201).json({ message: 'Usuario registrado con éxito' });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  const { username, email, password, role } = req.body; // Añade 'role' en el cuerpo de la solicitud
+  try {
+    const user = new User({ username, email, password, role }); // Guarda el rol del usuario
+    await user.save();
+    res.status(201).json({ message: "Usuario registrado con éxito" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 exports.login = async (req, res) => {
@@ -19,8 +19,14 @@ exports.login = async (req, res) => {
         if (!user || !(await user.comparePassword(password))) {
             return res.status(401).json({ message: 'El usuario o contraseña es incorrecto' });
         }
-        const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Añade el rol en el token
-        res.json({ token, userId: user._id, role: user.role }); // Incluye el rol en la respuesta
+        const expiresIn = 3600; // 1 hora en segundos
+        const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: expiresIn });
+        res.json({ 
+            token, 
+            userId: user._id, 
+            role: user.role,
+            expiresIn: expiresIn // Enviamos la duración del token en segundos
+        });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
